ffa66403228d858798cd06c94addf762bd4b062c
Sanskar Santosh Totla <sanskartotla22@gmail.com>
2021-07-02 18:44:20 +0530

    UI Improvements

--- components/navbar.vue
+++ components/navbar.vue
@@ -19,7 +19,26 @@
           </li>
         </ul>
       </transition>
-      <div v-if="auth" class="w-28 inline-block relative">
+      <div v-if="auth" class="inline-block relative">
+        <button
+          style="margin-right: 1vw"
+          type="button"
+          class="
+            bg-transparent
+            text-white
+            hover:bg-gray-200 hover:text-black
+            border border-white
+            hover:border-transparent
+            rounded
+            px-2
+            py-2
+          "
+        >
+          <span id="profile-name" class="todo-profile-name flex items-center">
+            {{ name }}
+            <img :src="'https://ui-avatars.com/api/?name=' + this.name + '&background=random&size=35&rounded=true&bold=true&font-size=0.6'" class="ml-2">
+          </span>
+        </button>
         <div class="group inline-block relative">
           <a
             class="
@@ -46,15 +65,40 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
+  data() {
+    return {
+      name: '',
+    }
+  },
   computed: {
     auth() {
       return this.$store.getters.auth
     },
   },
+  mounted() {
+    if(this.auth) {
+      this.getName()
+    }
+  },
   methods: {
     logout() {
       this.$store.commit('setToken', null)
-      this.$router.replace('/login/')
+      this.$router.push('/login/')
+    },
+
+    async getName() {
+      const headers = {
+        Authorization: 'Token ' + this.$store.getters.token,
+      }
+
+      await this.$axios
+        .get('auth/profile/', { headers })
+        .then((data) => {
+          this.name = data.data.name
+        })
+        .catch(() => {
+          this.$toast.error('Some error occurred due to which your name and icon might not match :(')
+        })
     },
   },
 })
